89feb0829572e6b927f845b33318d7628b8bbe18
ring04h <ring04h@gmail.com>
2016-05-16 16:25:04 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ import socks
 
 buffsize = 4096 
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "106.75.199.107", 19191) 
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 19191) 
 socket.socket = socks.socksocket
 
 def tcp_client(rhost, rport):
@@ -20,14 +20,14 @@ def udp_client(rhost, rport):
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     return client, rhost, rport
 
-# tcp_sock = tcp_client('42.62.62.62', 53)
+# tcp_sock = tcp_client('127.0.0.1', 53)
 # tcp_clnt,tcp_host,tcp_port = tcp_sock
 # tcp_clnt.connect((tcp_host,tcp_port))
 # while True:
 #     tcp_data = tcp_clnt.recv(buffsize)
 #     print repr(tcp_data)
 
-udp_conn = udp_client('42.62.52.62', 53)
+udp_conn = udp_client('127.0.0.1', 53)
 udp_clnt,udp_host,udp_port = udp_conn
 udp_clnt.sendto('client\n',(udp_host,udp_port))
 udp_clnt.sendto('client2\n',(udp_host,udp_port))
